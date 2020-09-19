edf20c765e8b396ea3f640641771a888e5fe6b2e
Binh Nguyen <Binh.dai.nguyen.97@gmail.com>
2020-09-19 05:51:08 -0700

    added users, mondgo local host support, added routes for admin creation

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,23 @@
 import express from 'express';
 import data from './data';
+import config from './config';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import userRoute from './routes/userRoutes';
+
+dotenv.config;
+
 const port = 5000;
 const app = express ();
+
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).catch(error => console.log(error.reason));
+
+app.use("/api/users", userRoute)
 
 app.get("/api/products/:id", (req, res) => {
     const productId = req.params.id;
