e760c4ec366d9bdc614884e5a6eba59112a6ac47
Binh Nguyen <Binh.dai.nguyen.97@gmail.com>
2020-09-19 04:20:18 -0700

    inital commit

---
+++ frontend/src/store.js
@@ -0,0 +1,20 @@
+import {createStore, combineReducers, compose, applyMiddleware} from 'redux';
+import { productListReducer, productDetailsReducer } from './reducers/productReducers';
+import Cookie from 'js-cookie';
+import thunk from 'redux-thunk';
+import {cartReducer} from './reducers/cartReducers';
+
+const cartItems = Cookie.getJSON("cartItems") || [];
+
+const initialState = { cart: {cartItems}};
+const reducer = combineReducers( {
+    productList: productListReducer,
+    productDetails: productDetailsReducer,
+    cart: cartReducer
+})
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
+
+export default store;
