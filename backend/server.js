1538d24331692ec2fc9b4f2218061938e31c6fdf
Binh Nguyen <Binh.dai.nguyen.97@gmail.com>
2020-09-19 08:50:35 -0700

    LOL BROKEN

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ import data from './data';
 import config from './config';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 import userRoute from './routes/userRoutes';
+import productRoute from './routes/productRoutes';
 
 dotenv.config;
 
@@ -17,23 +19,11 @@ mongoose.connect(mongodbUrl, {
     useCreateIndex: true
 }).catch(error => console.log(error.reason));
 
-app.use("/api/users", userRoute)
+app.use(bodyParser.json)
 
-app.get("/api/products/:id", (req, res) => {
-    const productId = req.params.id;
-    const product = data.products.find(x => x._id === productId);
-    if (product) {
-        res.send(product);
-    }
-    else {
-        res.status(404).send({msg: "Product Not Found."})
-    }
-});
+app.use("/api/users", userRoute);
 
-app.get("/api/products", (req, res) => {
-    res.send(data.products);
-});
-
+app.use("/api/products", productRoute);
 
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`) 
